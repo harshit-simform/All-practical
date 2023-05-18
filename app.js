720654ce782c835a6d69d0478c60dad653f501e8
harshit-simform <harshit.g@simformsolutions.com>
2023-05-18 18:59:54 +0530

    feat: staretd with phase 2

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ inquirer
     const meridian = timeDay[2];
 
     const foundDay = shop_schedule.find((element) => element.day === day);
+    const index = shop_schedule.findIndex((element) => element.day === day);
+    console.log(index);
 
     if (!foundDay) {
       console.log("closed");
@@ -36,11 +38,33 @@ inquirer
         `01/02/2000 ${new Date().toLocaleTimeString()}`
       ).getTime();
 
+      console.log();
+
       if (currentTime >= shopOpenTime && currentTime <= shopCloseTime) {
-        console.log("open");
+        const timeRemainForClose = new Date(shopCloseTime - currentTime);
+        console.log(
+          `Shop is open for another ${timeRemainForClose.getUTCHours()}hr : ${timeRemainForClose.getUTCMinutes()}min`
+        );
         return;
+      } else if (currentTime < shopOpenTime) {
+        const timeRemainForOpen = new Date(shopOpenTime - currentTime);
+        console.log(
+          `Shop is closed will open after ${timeRemainForOpen.getUTCHours()}hr : ${timeRemainForOpen.getUTCMinutes()}min`
+        );
       } else {
-        console.log("closed");
+        const nextDayIndex = index + 1;
+        console.log();
+        const shopOpenTime = new Date(
+          `01/02/2000 ${shop_schedule[nextDayIndex].open}`
+        ).getTime();
+        const timeRemainForOpen = new Date(shopOpenTime - currentTime);
+
+        console.log(
+          timeRemainForOpen.getUTCHours(),
+          timeRemainForOpen.getUTCMinutes()
+        );
       }
     }
   });
+
+function findNextOpenDay() {}
