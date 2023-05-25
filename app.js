b1deaf8deadb4d5b76323cbf36ae8504c8cdb52e
harshit-simform <harshit.g@simformsolutions.com>
2023-05-25 16:24:38 +0530

    feat: started with implementing the user authentication

---
+++ app.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(`${process.env.MONGODB_URL}`).then(() => {
+  console.log("connection to database is established");
+});
